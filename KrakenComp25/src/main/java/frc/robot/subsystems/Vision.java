f3d40ea4c0d0f8f9d4fe297c74e550b7807efe34
sai <saahilfardhook@gmail.com>
2025-01-16 16:16:00 -0800

    RobotContainer turned into singleton

--- KrakenComp25/src/main/java/frc/robot/subsystems/Vision.java
+++ KrakenComp25/src/main/java/frc/robot/subsystems/Vision.java
@@ -7,14 +7,30 @@ package frc.robot.subsystems;
 import java.lang.constant.DirectMethodHandleDesc.Kind;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
   /** Creates a new Vision. */
   private final PhotonCamera camera = new PhotonCamera(getName());
+  AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+  private final Transform3d cameraToRobot = new Transform3d(new Translation3d(0, 0, 0), new Rotation3d());
+  private Pose3d _pose;
+  
   public Vision() {}
+
+  public Pose3d UpdatePose(){
+    var result = camera.getAllUnreadResults();
+    Pose3d currentPose = PhotonUtils.estimateFieldToRobotAprilTag(cameraToRobot, _pose, cameraToRobot);
+    return currentPose;
+  }
 
   @Override
   public void periodic() {
