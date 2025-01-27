b43449cc8dfbb70e1032657fcd30c0ef1d230fa7
sai <saahilfardhook@gmail.com>
2025-01-26 21:18:54 -0800

    more vision pose stuff

--- KrakenComp25/src/main/java/frc/robot/subsystems/Vision.java
+++ KrakenComp25/src/main/java/frc/robot/subsystems/Vision.java
@@ -13,7 +13,7 @@ import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonUtils;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.targeting.PhotonPipelineResult;
-
+import org.littletonrobotics.junction.Logger;
 import com.ctre.phoenix6.Utils;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
@@ -34,36 +34,40 @@ public class Vision extends SubsystemBase {
   /** Creates a new Vision. */
   private final PhotonCamera camera = new PhotonCamera(getName());
 
-  //Pose estimation
+  // Pose estimation
   AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
   private final Transform3d cameraToRobot = new Transform3d(new Translation3d(0, 12.25, 0), new Rotation3d());
   private Transform3d cameraToTarget;
-  private Pose3d _pose;
-  PhotonPoseEstimator poseEstimator = new PhotonPoseEstimator(fieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, cameraToRobot);
+  private Pose2d _pose;
+  PhotonPoseEstimator poseEstimator = new PhotonPoseEstimator(fieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+      cameraToRobot);
 
-  //references from robot container
+  // references from robot container
   RobotContainer rc = RobotContainer.getInstance();
   CommandSwerveDrivetrain driveTrain = rc.drivetrain;
   double forward = -rc.driver.getLeftY() * rc.MaxSpeed;
   double strafe = -rc.driver.getLeftY() * rc.MaxSpeed;
   double steer = -rc.driver.getLeftY() * rc.MaxAngularRate;
 
-
   public Vision() {
   }
-  // public Optional<EstimatedRobotPose> getEsti
 
-  // public void UpdatePose() {
-  //   Transform3d photonTransform;
-  //   var result = camera.getLatestResult();
-  //   //   var target = result.getultiTagResult();
-  //   }
+  public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
+    Optional<EstimatedRobotPose> visionEst = Optional.empty();
+    for (var change : camera.getAllUnreadResults()) {
+        visionEst = poseEstimator.update(change);
+    }
+    return visionEst;
+}
 
-  //   Pose3d rawPose = PhotonUtils.estimateFieldToRobotAprilTag(cameraToRobot, _pose, cameraToRobot);
-  //   Pose2d filteredPose = new Pose2d(new Translation2d(rawPose.getX(), rawPose.getY()),
-  //       rawPose.getRotation().toRotation2d());
-  //   // driveTrain.addVisionMeasurement(filteredPose, Utils.getCurrentTimeSeconds());
-  // }
+
+  public void UpdatePose() {
+    var visionEst = getEstimatedGlobalPose();
+    visionEst.ifPresent(est -> {
+      driveTrain.addVisionMeasurement(est.estimatedPose.toPose2d(), est.timestampSeconds);
+      _pose = est.estimatedPose.toPose2d();
+    });
+  }
 
   // TODO: Find translation and rotation distances from april tags
   // TODO: Find how to drive request to position based off of april tags
@@ -73,12 +77,12 @@ public class Vision extends SubsystemBase {
     double targetYaw = 0.0;
     var results = camera.getAllUnreadResults();
     if (!results.isEmpty()) {
-      var result = results.get(results.size() -1);
-      if (result.hasTargets()){
+      var result = results.get(results.size() - 1);
+      if (result.hasTargets()) {
         System.out.println("results found");
 
-        for(var target : result.getTargets()){
-          if(target.getFiducialId() == 1){
+        for (var target : result.getTargets()) {
+          if (target.getFiducialId() == 1) {
             targetYaw = target.getYaw();
             targetRange = PhotonUtils.calculateDistanceToTargetMeters(targetYaw, targetYaw, targetRange, targetYaw);
           }
@@ -90,5 +94,7 @@ public class Vision extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    UpdatePose();
+    Logger.recordOutput("VisionPose", _pose);
   }
 }
