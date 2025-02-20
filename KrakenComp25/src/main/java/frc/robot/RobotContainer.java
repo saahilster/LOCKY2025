0a419b1c7a00929c74691899069252efd65428f5
sai <saahilfardhook@gmail.com>
2025-02-19 20:31:05 -0800

    Temporary Fix

--- KrakenComp25/src/main/java/frc/robot/RobotContainer.java
+++ KrakenComp25/src/main/java/frc/robot/RobotContainer.java
@@ -129,22 +129,24 @@ public class RobotContainer {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
-                // Drivetrain will execute this command periodically
-                drivetrain.applyRequest(() -> drive.withVelocityX(-driver.getLeftY() * MaxSpeed) // Drive forward with
-                                                                                                 // negative Y (forward)
-                        .withVelocityY(-driver.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                        .withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive counterclockwise with
-                                                                                  // negative X (left)
-                ));
+            // Drivetrain will execute this command periodically
+            drivetrain.applyRequest(() -> drive.withVelocityX(driver.getLeftY() * MaxSpeed) // Drive forward with
+                                                                                             // negative Y (forward)
+                    .withVelocityY(driver.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive counterclockwise with
+                                                                              // negative X (left)
+            ));
 
-        slowButton.whileTrue(drivetrain.applyRequest(() -> drive.withVelocityX(-driver.getLeftY() * 0.9) // Drive
-                                                                                                       // forward with
-                                                                                                       // negative Y
-                                                                                                       // (forward)
-                .withVelocityY(-driver.getLeftX() * 1.5) // Drive left with negative X (left)
-                .withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X
-                                                                          // (left)
+    
+            slowButton.whileTrue(drivetrain.applyRequest(() -> drive.withVelocityX(driver.getLeftY() * 1) // Drive
+                                                                                                     // forward with
+                                                                                                     // negative Y
+                                                                                                     // (forward)
+            .withVelocityY(driver.getLeftX() * 1.5) // Drive left with negative X (left)
+            .withRotationalRate(-driver.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X
+                                                                      // (left)
         ));
+
         slowButton.whileTrue(new RunCommand(()-> ledSub.TestLED(240, 200, 90), ledSub));
         slowButton.whileFalse(new RunCommand(()-> ledSub.TestLED(0, 0, 0), ledSub));
 
